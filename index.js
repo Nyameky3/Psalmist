2ca973ab561b9e009247ae045bae9ab0513c2d87
Nyameky3 <kwabenanyameky3@gmail.com>
2021-03-21 23:47:23 +0000

    added JS for easier navigation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let read = document.getElementById("read");
 let overlay = document.getElementById("overlay");
 let modal = document.getElementById("hidden");
+let contactBtn = document.getElementById("contact");
+let contacts = document.getElementById("contacts");
 
 read.addEventListener("click", openModal);
 overlay.addEventListener("click", closeModal);
+contactBtn.addEventListener("click", indicate);
 
 function openModal() {
     overlay.classList.add("active");
@@ -13,4 +16,11 @@ function openModal() {
 function closeModal() {
     overlay.classList.remove("active");
     hidden.classList.remove("active");
+}
+
+function indicate() {
+    contacts.classList.add("active");
+    setTimeout(() => {
+        contacts.classList.remove("active");
+    }, 1000);
 }
